29e6d1e3f7f8462c3420345bf39ac9235d6b088d
cacayue <a15453606@outlook.com>
2020-08-24 07:36:19 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mokf6j
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mokf6j)
---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+import { CartService } from '../cart.service';
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+  items;
+  checkoutForm;
+  constructor(
+    private cartService: CartService,
+    private form: FormBuilder
+  ) { 
+    this.checkoutForm = this.form.group({
+      name:'',
+      address:''
+    })
+  }
+
+  ngOnInit() {
+    this.items = this.cartService.getItems();
+  }
+
+  onSubmit(customerData){
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+    console.warn('Your order has been submitted', customerData);
+  }
+}
